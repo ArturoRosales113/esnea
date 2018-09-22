2d1ceb822021a291e91dcb6823e18b53ab668637
ArturoRosales113 <arturoi_rosales@hotmail.com>
2018-09-22 13:57:32 -0500

    banner de testimonios

--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -136,7 +136,7 @@
 <!-- /Texto Bienvendia -->
 
 <!-- Testimonios -->
-<div class="row py-5 bg2 mt-5 align-items-center" style="min-height:25vh;">
+<!-- <div class="row py-5 bg2 mt-5 align-items-center" style="min-height:25vh;">
  <div class="col-lg-6 text-center">
   <h2 class="font1">
    <small class="text-white">Ciclo 2018-2019</small><br>
@@ -147,8 +147,8 @@
   <p>Infórmate a tiempo y descarga la convocatorioa para el nuevo ciclo escolar</p>
   <a href="#" class="btn btn-outline-light">Descargar PDF</a>
  </div>
-</div>
-<!-- <div class="row align-items-center justify-content-end" style="height:100vh; background-image:url('{{asset('img/testimonio.jpg')}}');background-size:cover;background-repeat:no-repeat;;">
+</div> -->
+<div class="row align-items-center justify-content-end" style="height:100vh; background-image:url('{{asset('img/testimonio.jpg')}}');background-size:cover;background-repeat:no-repeat;;">
  <div class="col-md-7">
   <span class="rem3 font1 text-white text-uppercase">
    “APRENDÍ A UTILIZAR LOS RECURSOS <br>
@@ -159,7 +159,7 @@ PARA BENEFICIO DE LA COMUNIDAD”
   <br>
   <small class="text-white">Egresada 2015 de la carrera de Ecoturismo <br> y Administradora del Eco-Hotel "La Ruta"</small>
  </div>
-</div> -->
+</div>
 
 <!-- Testimonios -->
 
